6e49f42714dbce982d5006bdeb38309182f86f42
aakashsr <aakashsri.dev@gmail.com>
2019-08-31 23:18:47 +0530

    added styles to completed items

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,12 +1,22 @@
 import React from "react";
 
-const TodoItem = ({item,handleConformation}) => {
+const TodoItem = ({ item, handleConformation }) => {
+  const completedStyle = {
+    fontStyle: "italic",
+    color: "#cdcdcd",
+    textDecoration: "line-through"
+  };
+
   return (
     <div className="todo-item">
-      <input onChange={handleConformation} type="checkbox" checked={item.completed}/>
-      <p>{item.text}</p>
+      <input
+        onChange={handleConformation}
+        type="checkbox"
+        checked={item.completed}
+      />
+      <p style={item.completed ? completedStyle : null}>{item.text}</p>
     </div>
   );
-}
+};
 
 export default TodoItem;
